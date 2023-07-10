e555501b3ceb975b1eb1a6081dec0b424e420b50
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-07-10 01:14:52 -0300

    A button has been made on Board.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import './App.css';
 import Footer from './components/Footer/Footer';
 import Navigator from './components/Navigator/Navigator';
 import RoutesComponent from "./routes/RoutesComponent";
+import { useNavigate } from "react-router-dom";
 
 function App() {
-
   return (
     <>
       <Navigator></Navigator>
@@ -13,7 +13,7 @@ function App() {
       </main>
       <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,6 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 const Board = () => {
+    const navigate = useNavigate();
+    
     return (
-        <div>Board</div>
+        <>
+            <section>
+
+            </section>
+            <button className="btn btn-white" type="button" onClick={()=> navigate("/")}>Volver</button>
+        </>
     )
 }
 
