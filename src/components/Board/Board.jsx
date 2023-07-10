feafe60dca79dbb73be38ab491f98e53c1791864
Diego Vaca Paz <diegovacapaz@gmail.com>
2023-07-10 14:02:43 -0300

    Board grid styles developed

--- src/components/Board/Board.jsx
+++ src/components/Board/Board.jsx
@@ -1,14 +1,27 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import Cell from "./Cell/Cell";
+import "./Board.css";
 
 const Board = () => {
+    const [board,setBoard] = useState(Array(9).fill(undefined));
+
     const navigate = useNavigate();
     
     return (
         <>
-            <section>
-
+            <section className="game">
+                <article className="container">
+                    <div className="board">                    
+                        {board.map((cell,index)=>{
+                            return <Cell key={index}></Cell>
+                        })}
+                    </div>
+                </article>
             </section>
-            <button className="btn btn-white" type="button" onClick={()=> navigate("/")}>Volver</button>
+            <section className="center">
+                <button className="btn btn-white" type="button" onClick={()=> navigate("/")}>Volver</button>
+            </section>
         </>
     )
 }
---
+++ src/components/Board/Cell/Cell.jsx
@@ -0,0 +1,11 @@
+const Cell = () => {
+    return (
+        <div className="cell">
+            <article>
+                <p>Hola</p>
+            </article>
+        </div>
+    );
+}
+
+export default Cell;
